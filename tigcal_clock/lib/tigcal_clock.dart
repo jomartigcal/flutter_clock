ac5653f895367e15dd85acbfd7497bf6b1b11f8a
Jomar Tigcal <jomar@tigcal.com>
2020-01-15 20:17:48 -0800

    Updating clock hands

--- tigcal_clock/lib/tigcal_clock.dart
+++ tigcal_clock/lib/tigcal_clock.dart
@@ -10,7 +10,6 @@ import 'package:flutter/semantics.dart';
 import 'package:intl/intl.dart';
 import 'package:vector_math/vector_math_64.dart' show radians;
 
-import 'container_hand.dart';
 import 'drawn_hand.dart';
 
 /// Total distance traveled by a second or a minute hand, each second or minute,
@@ -114,35 +113,24 @@ class _TigcalClockState extends State<TigcalClock> {
         color: customTheme.backgroundColor,
         child: Stack(
           children: [
-            // Example of a hand drawn with [CustomPainter].
             DrawnHand(
               color: customTheme.accentColor,
               thickness: 4,
-              size: 1,
+              size: 0.7,
               angleRadians: _now.second * radiansPerTick,
             ),
             DrawnHand(
               color: customTheme.highlightColor,
-              thickness: 16,
-              size: 0.9,
+              thickness: 8,
+              size: 0.5,
               angleRadians: _now.minute * radiansPerTick,
             ),
-            // Example of a hand drawn with [Container].
-            ContainerHand(
-              color: Colors.transparent,
-              size: 0.5,
+            DrawnHand(
+              color: customTheme.primaryColor,
+              thickness: 10,
+              size: 0.3,
               angleRadians: _now.hour * radiansPerHour +
                   (_now.minute / 60) * radiansPerHour,
-              child: Transform.translate(
-                offset: Offset(0.0, -60.0),
-                child: Container(
-                  width: 32,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    color: customTheme.primaryColor,
-                  ),
-                ),
-              ),
             ),
           ],
         ),
